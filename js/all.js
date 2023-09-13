23516956671dacf4c154b65ca25f805cbca62017
Yusupov Elbek <elbeky829@gmail.com>
2023-09-13 18:21:16 +0500

    all posts added

---
+++ js/all.js
@@ -0,0 +1,31 @@
+// const axios = require("axios");
+
+let url = 'http://localhost:3000/posts'
+
+async function showPosts(){
+    let response = await fetch(url)
+    let result = await response.json()
+    console.log(result);
+    let str = ''
+    result.map((item) => {
+        str+=`
+        <a href="#">
+        <div class="card">
+        <div class="img"><img src='${item.img}' alt="" width: 547.295px;
+        height: 318px;></div>
+        <div class="card__content">
+          <h3>${item.category}</h3>
+          <h2>${item.title}</h2>
+          <p>${item.description}</p>
+        </div>
+      </div>
+      </a>
+        `
+    })
+    document.querySelector('.all__posts__card').innerHTML = str
+}
+showPosts()
+
+let search = document.querySelector('#all-posts-search')
+
+search.addEventListener('keyup', )
